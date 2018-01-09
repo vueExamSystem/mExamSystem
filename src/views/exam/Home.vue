6d3481135a280808e6d2823694e42cd111c0a764
hytcwangfang <wendy19921214>
2018-01-09 23:46:33 +0800

    倒计时相关算法

--- src/views/exam/Home.vue
+++ src/views/exam/Home.vue
@@ -9,13 +9,13 @@
 				<div class="content">
 					<ul class="section-list">
 						<li v-for="exam in examList">
-							<template v-if="isTodayExam(exam.time)">
+							<template v-if="isTodayExam(exam.time) && isValid(exam.time)">
 								<p>{{exam.name}}</p>
 								<div>
 									<div class="time-down">
 										<img src="/static/images/clock.png" width="15">
 										<span>倒计时：</span>
-										<span class="time">{{exam.time | remainTime}}</span>
+										<span class="time">{{remainTime(exam.time)}}</span>
 									</div>
 									<mt-button v-show="isShouldExam(exam.time)" class="pull-right" type="primary" @click="toExam(exam.id)">进入考试</mt-button>
 								</div>
@@ -30,6 +30,9 @@
 										<img src="/static/images/clock.png" width="15">
 										<span>{{exam.time}}</span>
 									</div>
+								</div>
+								<div class="flag" v-if="isTodayExam(exam.time)">
+									<span>今日</span>
 								</div>
 							</template>
 						</li>
@@ -54,15 +57,15 @@
 				examList:[{
 					id: '1',
 					name: '大一物理（上）期中考试',
-					time: '2018/01/09 15:00'
+					time: '2018/01/09 23:32'
 				},{
 					id: '2',
 					name: '大一高等数学（上）期中考试',
-					time: '2018/01/09 16:00'
+					time: '2018/01/09 23:40'
 				},{
 					id: '3',
 					name: '大一大学英语（上）期中考试',
-					time: '2018/01/09 18:00'
+					time: '2018/01/09 23:50'
 				},{
 					id: '3',
 					name: '大一计算机（上）期中考试',
@@ -76,11 +79,6 @@
 				return this.getDateObj(this.nowDate);
 			}
 		},
-		filters:{
-			remainTime(dateString){
-				return '1'
-			}
-		},
 		methods:{
 			/**string转换为date
 			*dateString:2018/01/09 18:00
@@ -88,34 +86,57 @@
 			dateParse(dateString){
 				return new Date(dateString);
 			},
-			/**
-			*获取date相关年月日等
-			*/
-			getDateObj(date){
-				var year = date.getFullYear();
-				var month = date.getMonth() + 1;
-				var day = date.getDate() + 1;
-				var hour = date.getHours();
-				var minute = date.getMinutes();
-				var second = date.getSeconds();
-				return {
-					date,
-					year,
-					month,
-					day,
-					hour,
-					minute,
-					second,
-					string:year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second
-				};
+			remainTime(dateString){//倒计时
+				var hourStr = '0';
+				var minuteStr = '0';
+				var secondStr = '0';
+				var thisD = this.dateParse(dateString);
+				var minuteSeconds = 1000 * 60;//一分钟毫秒数
+				var hourSeconds = minuteSeconds * 60;//一小时毫秒数
+				var totalSeconds = (thisD.getTime() - this.nowDate.getTime());//剩余总秒数
+				var remainHours = Math.floor(totalSeconds / hourSeconds);//对应剩余小时
+				var remainMinutes = Math.floor(totalSeconds % hourSeconds / minuteSeconds);//对应剩余分钟
+				var remainSeconds = Math.floor(totalSeconds % hourSeconds % minuteSeconds / 1000);//对应剩余秒
+				if(remainHours < 10){//剩余小时小于10小时
+					hourStr += remainHours;
+				}else{
+					hourStr = remainHours + '';
+				}
+				if(remainMinutes < 10){//剩余分钟小于10分钟
+					minuteStr += remainMinutes;
+				}else{
+					minuteStr = remainMinutes + '';
+				}
+				if(remainSeconds < 10){//剩余秒小于10秒
+					secondStr += remainSeconds;
+				}else{
+					secondStr = remainSeconds + '';
+				}
+				return hourStr + ':' + minuteStr + ':' + secondStr;
+			},
+			isShouldExam(dateString){//是否可以进入考试
+				var isShouldExam = false;
+				var thisD = this.dateParse(dateString);
+				var crisis = 1000 * 60 * 5 + 1000;//5分钟
+				var totalSeconds = (thisD.getTime() - this.nowDate.getTime());
+				if(totalSeconds <= crisis){
+					isShouldExam = true;
+				}
+				return isShouldExam;
 			},
 			toExam(id){//进入考试页面
 
 			},
-			isShouldExam(dateString){//是否可以进入考试
-
+			isValid(dateString){//是否没有过期
+				var isInvalid = true;
+				var thisD = this.dateParse(dateString);
+				var totalSeconds = (thisD.getTime() - this.nowDate.getTime());
+				if(totalSeconds <= 0){
+					isInvalid = false;
+				}
+				return isInvalid;
 			},
-			isTodayExam(dateString){
+			isTodayExam(dateString){//是否今天考
 				var isToday = 0;
 				var current = this.getDateObj(this.dateParse(dateString));
 				//只初始运行
@@ -125,6 +146,27 @@
 				}
 				return isToday;
 			},
+			/**
+ 			*获取date相关年月日等
+ 			*/
+ 			getDateObj(date){
+ 				var year = date.getFullYear();
+ 				var month = date.getMonth() + 1;
+ 				var day = date.getDate() + 1;
+ 				var hour = date.getHours();
+ 				var minute = date.getMinutes();
+ 				var second = date.getSeconds();
+ 				return {
+ 					date,
+ 					year,
+ 					month,
+ 					day,
+ 					hour,
+ 					minute,
+ 					second,
+ 					string:year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second
+ 				};
+ 			},
 			showSearch(){//展示搜索页面
 				this.isSearchVisible = true;
 			},
@@ -134,7 +176,6 @@
 			timeClockRun(){
 				setInterval(()=>{
 					this.nowDate = new Date();
-					// console.log(new Date())
 				},1000);
 			}
 		},
