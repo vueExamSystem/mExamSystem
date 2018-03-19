aafdc8fa95ed14d1802a290a3e826b874a1eb3b3
童鑫 <naxinaa123@163.com>
2018-03-19 16:51:31 +0800

    改成window.localStorage存储token

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-    token: window.sessionStorage.getItem('token') || '',
+    token: window.localStorage.getItem('token') || '',//window.sessionStorage.getItem('token') || '',
     isNavVisible: 1//底部导航是否可见
 };
 const getters = {
@@ -12,11 +12,11 @@ const getters = {
 };
 const mutations = {
   SET_TOKEN(state,payload){
-      window.sessionStorage.setItem('token', payload.token);
+    window.localStorage.setItem('token', payload.token);//window.sessionStorage.setItem('token', payload.token);
     state.token = payload.token;
   },
   LOG_OUT(state){
-      window.sessionStorage.removeItem('token');
+     window.localStorage.removeItem('token');//window.sessionStorage.removeItem('token');
     state.token = '';
   },
   SHOW_NAV(state){
